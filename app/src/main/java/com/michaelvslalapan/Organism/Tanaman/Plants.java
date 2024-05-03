a98fee57d476e8c8d44be544c3d7c70f21c0eb67
silalahi20 <josiasilalahi2004@gmail.com>
2024-05-03 16:23:46 +0700

    add abstract class plant

--- app/src/main/java/com/michaelvslalapan/Organism/Tanaman/Plants.java
+++ app/src/main/java/com/michaelvslalapan/Organism/Tanaman/Plants.java
@@ -19,6 +19,72 @@ public class Plants {
         this.range = range;
         this.cooldown = cooldown;
     }
-    
+    //Getter Method
+    public String get_Name(){
+        return name;
+    }
+
+    public int get_Cost(){
+        return cost;
+    }
+
+    public int get_Health(){
+        return health;
+    }
+
+    public int  get_Attack_Damage(){
+        return attackDamage;
+    }
+
+    public int get_Attack_Speed(){
+        return attackSpeed;
+    }
+
+    public int get_Range(){
+        return range;
+    }
+
+    public int get_Cooldown(){
+        return cooldown;
+    }
+
+    // Setter methods
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setAttackDamage(int attackDamage) {
+        this.attackDamage = attackDamage;
+    }
+
+    public void setAttackSpeed(int attackSpeed) {
+        this.attackSpeed = attackSpeed;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    public void setCooldown(int cooldown) {
+        this.cooldown = cooldown;
+    }
+
+    // Method untuk menyerang zombie
+    public void attackZombie(/** */) {
+        // Implementasi klo nyerang zombie
+    }
+
+    // Method untuk mengurangi health
+    public void decreaseHealth() {
+        // Implementasi pengurangan health akibat attack dari zombie
+    }
 
 }
