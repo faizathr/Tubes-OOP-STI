547b653b0c538024bdb3a76810b7403fb146a73c
David Dewanto <david.dewanto@gmail.com>
2024-05-06 18:50:20 +0700

    Add Spek Tubes Attributes

--- app/src/main/java/com/michaelvslalapan/Organism/Tanaman/PlantInventory.java
+++ app/src/main/java/com/michaelvslalapan/Organism/Tanaman/PlantInventory.java
@@ -1,11 +1,12 @@
 package com.michaelvslalapan.Organism.Tanaman;
 import java.util.ArrayList;
+import com.michaelvslalapan.Organism.Tanaman.Plants;
 
 public class PlantInventory {
-    private final ArrayList<Plant> plantCatalog;
+    private final ArrayList<Plants> plantCatalog;
 
     public PlantInventory() {
-        plantCatalog = new ArrayList<>();
+        plantCatalog = new ArrayList<Plants>();
         initializeCatalog();
     }
 
@@ -14,12 +15,12 @@ public class PlantInventory {
         plantCatalog.add(new Sunflower());
         plantCatalog.add(new Peashooter());
         plantCatalog.add(new Wallnut());
-        plantCatalog.add(new CherryBomb());
         plantCatalog.add(new Lilypad());
         plantCatalog.add(new Squash());
+        plantCatalog.add(new SnowPea());
     }
 
-    public ArrayList<Plant> getPlantCatalog() {
+    public ArrayList<Plants> getPlantCatalog() {
         return plantCatalog;
     }
 }
--- app/src/main/java/com/michaelvslalapan/Organism/Tanaman/Squash.java
+++ app/src/main/java/com/michaelvslalapan/Organism/Tanaman/Squash.java
@@ -1,5 +1,9 @@
 package com.michaelvslalapan.Organism.Tanaman;
 
-public class Squash {
+public class Squash extends Plants {
+
+    public Squash(){
+        super("Squash", 50, 100, 5000, 0, 1, 20, false);
+    }
     
 }
