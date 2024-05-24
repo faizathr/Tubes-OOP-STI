3d0673b67278ba605dd3025ae852a633f59b4f6d
faizathr <121447622+faizathr@users.noreply.github.com>
2024-05-24 19:45:31 +0700

    Add PoleVault & DolphinRider Jump & 4 optional Zombies

--- app/src/main/java/com/michaelvslalapan/Organism/Zombie/PoleVaultingZombie.java
+++ app/src/main/java/com/michaelvslalapan/Organism/Zombie/PoleVaultingZombie.java
@@ -2,6 +2,6 @@ package com.michaelvslalapan.Organism.Zombie;
 
 public class PoleVaultingZombie extends Zombie {
     public PoleVaultingZombie(int LaneY){
-        super(3, "Pole Vaulting Zombie", 175, 100, 1, false, LaneY);
+        super(2, "Pole Vaulting Zombie", 175, 100, 1, false, LaneY);
     }
 }
